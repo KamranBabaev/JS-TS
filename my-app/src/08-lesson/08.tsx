196fc064df14f194e9f222d18a2abf83a384e0c4
Kamran Babaev <babaevkamran2094@gmail.com>
2021-05-25 16:01:56 +0300

    +

---
+++ my-app/src/08-lesson/08.tsx
@@ -0,0 +1,8 @@
+let users = {
+    '101': {id: 101, name: 'Sam'},
+    '20': {id: 20, name: 'Nick'},
+    '31': {id: 31, name: 'John'},
+    '44': {id: 44, name: 'Andy'},
+}
+
+console.log(users["20"])
